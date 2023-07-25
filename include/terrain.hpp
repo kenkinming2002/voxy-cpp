191f808d29f5a361bff2d40605e35a56649cda11
KenKwok <kenkinming2002@gmail.com>
2023-07-25 13:54:08 +0800

    Multiple chunk generation threads

--- include/terrain.hpp
+++ include/terrain.hpp
@@ -8,33 +8,42 @@
 #include <glm/glm.hpp>
 #include <glm/gtx/hash.hpp>
 
+#include <unordered_set>
 #include <unordered_map>
+
+#include <variant>
+#include <optional>
 
 #include <mutex>
 #include <condition_variable>
 
 #include <thread>
 
-struct Terrain
+class Terrain
 {
-  gl::Program program;
-
-  std::mutex                            chunks_mutex;
-  std::unordered_map<glm::ivec2, Chunk> chunks;
-
-  std::condition_variable_any load_cv;
-  std::mutex                  load_mutex;
-  glm::vec2                   center;
-  float                       radius;
-
-  std::jthread worker;
-
+public:
   Terrain();
 
+public:
   void load(glm::vec2 center, float radius);
 
   void update(float dt);
   void render(const Camera& camera, const Lights& lights);
+
+private:
+  void loader(std::stop_token stoken);
+
+private:
+  gl::Program m_program;
+
+  std::mutex                  m_mutex;
+  std::condition_variable_any m_cv;
+
+  struct Pending {};
+  struct Loading {};
+  std::unordered_map<glm::ivec2, std::variant<Pending, Loading, Chunk>> m_states;
+
+  std::vector<std::jthread> m_workers;
 };
 
 #endif // TERRAIN_HPP
--- src/terrain.cpp
+++ src/terrain.cpp
@@ -2,82 +2,43 @@
 
 #include <glm/gtc/type_ptr.hpp>
 
+#include <algorithm>
+
 Terrain::Terrain() :
-  program(gl::compile_program("assets/chunk.vert", "assets/chunk.frag")),
-  chunks_mutex(),
-  chunks(),
-  load_cv(),
-  load_mutex(),
-  center(),
-  radius(500.0f),
-  worker([this](std::stop_token stoken){
-    std::unique_lock load_lk(load_mutex);
-    for(;;)
-    {
-      glm::vec2 center = this->center;
-      float     radius = this->radius;
-
-      load_lk.unlock();
-
-      std::vector<glm::ivec2> positions_chunk;
-
-      // 1: Get a list of chunks that need to be loaded
-      glm::ivec2 center_chunk = center / (float)Layer::WIDTH;
-      int        radius_chunk = radius / Layer::WIDTH;
-      for(int yoffset_chunk = -radius_chunk; yoffset_chunk <= radius_chunk; ++yoffset_chunk)
-        for(int xoffset_chunk = -radius_chunk; xoffset_chunk <= radius_chunk; ++xoffset_chunk)
-        {
-          glm::ivec2 offset_chunk   = glm::ivec2(xoffset_chunk, yoffset_chunk);
-          glm::ivec2 position_chunk = center_chunk + offset_chunk;
-
-          if(offset_chunk.x * offset_chunk.x + offset_chunk.y * offset_chunk.y > radius_chunk)
-            continue;
-
-          std::lock_guard chunks_lk(chunks_mutex);
-          if(chunks.contains(position_chunk))
-            continue;
-
-          positions_chunk.push_back(position_chunk);
-        }
-
-      // 2: Load them one by one
-      for(glm::ivec2 position_chunk : positions_chunk)
-      {
-        Chunk chunk(position_chunk);
-
-        std::unique_lock chunks_lk(chunks_mutex);
-        chunks.emplace(position_chunk, std::move(chunk));
-        chunks_lk.unlock();
-
-        if(stoken.stop_requested())
-          return;
-      }
-
-      load_lk.lock();
-
-      if(!load_cv.wait(load_lk, stoken, [&]() { return center != this->center || radius != this->radius; }))
-        return;
-    }
-  })
-{}
+  m_program(gl::compile_program("assets/chunk.vert", "assets/chunk.frag")),
+  m_mutex(),
+  m_cv(),
+  m_states(),
+  m_workers()
+{
+  unsigned count = std::thread::hardware_concurrency();
+  for(unsigned i=0; i<count; ++i)
+    m_workers.emplace_back(std::bind(&Terrain::loader, this, std::placeholders::_1));
+}
 
 void Terrain::load(glm::vec2 center, float radius)
 {
-  bool changed = false;
-  std::unique_lock load_lk(load_mutex);
-  if(this->center != center)
-  {
-    this->center = center;
-    changed = true;
-  }
-  if(this->radius != radius)
-  {
-    this->radius = radius;
-    changed = true;
-  }
-  load_lk.unlock();
-  if(changed)
-    load_cv.notify_one();
+  std::unique_lock lk(m_mutex);
+
+  glm::ivec2 center_chunk = center / (float)Layer::WIDTH;
+  int        radius_chunk = radius / Layer::WIDTH;
+
+  bool any = false;
+  for(int yoffset_chunk = -radius_chunk; yoffset_chunk <= radius_chunk; ++yoffset_chunk)
+    for(int xoffset_chunk = -radius_chunk; xoffset_chunk <= radius_chunk; ++xoffset_chunk)
+    {
+      glm::ivec2 offset_chunk   = glm::ivec2(xoffset_chunk, yoffset_chunk);
+      glm::ivec2 position_chunk = center_chunk + offset_chunk;
+      if(offset_chunk.x * offset_chunk.x + offset_chunk.y * offset_chunk.y > radius_chunk)
+        continue;
+
+      auto [it, success] = m_states.emplace(position_chunk, Pending{});
+      if(success)
+        any = true;
+    }
+
+  if(any)
+    m_cv.notify_all();
 }
 
 void Terrain::update(float dt)
@@ -90,26 +51,63 @@ void Terrain::render(const Camera& camera, const Lights& lights)
   glm::mat4 view       = camera.view();
   glm::mat4 projection = camera.projection();
 
-  glUseProgram(program);
+  glUseProgram(m_program);
   {
-    glUniform3fv(glGetUniformLocation(program, "viewPos"),        1, glm::value_ptr(camera.position));
-    glUniform3fv(glGetUniformLocation(program, "light.pos"),      1, glm::value_ptr(lights.light.pos));
-    glUniform3fv(glGetUniformLocation(program, "light.ambient"),  1, glm::value_ptr(lights.light.ambient));
-    glUniform3fv(glGetUniformLocation(program, "light.diffuse"),  1, glm::value_ptr(lights.light.diffuse));
+    glUniform3fv(glGetUniformLocation(m_program, "viewPos"),        1, glm::value_ptr(camera.position));
+    glUniform3fv(glGetUniformLocation(m_program, "light.pos"),      1, glm::value_ptr(lights.light.pos));
+    glUniform3fv(glGetUniformLocation(m_program, "light.ambient"),  1, glm::value_ptr(lights.light.ambient));
+    glUniform3fv(glGetUniformLocation(m_program, "light.diffuse"),  1, glm::value_ptr(lights.light.diffuse));
 
-    std::lock_guard chunks_lk(chunks_mutex);
-    for(const auto& [cpos, chunk] : chunks)
+    std::lock_guard guard(m_mutex);
+    for(const auto& [cpos, state] : m_states)
+      if(std::holds_alternative<Chunk>(state))
+      {
+        const Chunk& chunk = std::get<Chunk>(state);
+
+        glm::mat4 model  = glm::translate(glm::mat4(1.0f), glm::vec3( Layer::WIDTH * cpos.x, Layer::WIDTH * cpos.y, 0.0f));
+        glm::mat4 normal = glm::transpose(glm::inverse(model));
+        glm::mat4 MVP    = projection * view * model;
+
+        glUniformMatrix4fv(glGetUniformLocation(m_program, "MVP"),    1, GL_FALSE, glm::value_ptr(MVP));
+        glUniformMatrix4fv(glGetUniformLocation(m_program, "model"),  1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(glGetUniformLocation(m_program, "normal"), 1, GL_FALSE, glm::value_ptr(normal));
+
+        chunk.layers_mesh.draw();
+      }
+  }
+}
+
+void Terrain::loader(std::stop_token stoken)
+{
+  std::unique_lock lk(m_mutex);
+  for(;;)
+  {
+    glm::ivec2 cpos;
+    m_cv.wait(lk, stoken, [&]()
     {
-      glm::mat4 model  = glm::translate(glm::mat4(1.0f), glm::vec3( Layer::WIDTH * cpos.x, Layer::WIDTH * cpos.y, 0.0f));
-      glm::mat4 normal = glm::transpose(glm::inverse(model));
-      glm::mat4 MVP    = projection * view * model;
+      auto it = std::find_if(m_states.begin(), m_states.end(), [](auto& value)
+      {
+        auto& [_, state ] = value;
+        return std::holds_alternative<Pending>(state);
+      });
 
-      glUniformMatrix4fv(glGetUniformLocation(program, "MVP"),    1, GL_FALSE, glm::value_ptr(MVP));
-      glUniformMatrix4fv(glGetUniformLocation(program, "model"),  1, GL_FALSE, glm::value_ptr(model));
-      glUniformMatrix4fv(glGetUniformLocation(program, "normal"), 1, GL_FALSE, glm::value_ptr(normal));
+      if(it != m_states.end())
+      {
+        cpos = it->first;
+        return true;
+      }
+      else
+        return false;
+    });
 
-      chunk.layers_mesh.draw();
-    }
+    if(stoken.stop_requested())
+      return;
+
+    lk.unlock();
+    Chunk chunk(cpos);
+    lk.lock();
+
+    m_states.insert_or_assign(cpos, std::move(chunk));
   }
 }
 
