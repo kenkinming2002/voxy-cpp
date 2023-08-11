ee3129385abae9a4b80b64d371ba9893ab551762
KenKwok <kenkinming2002@gmail.com>
2023-08-11 19:47:45 +0800

    Remove unused Block::* constant

--- include/block.hpp
+++ include/block.hpp
@@ -8,16 +8,8 @@ struct Block
   static constexpr std::uint32_t ID_GRASS = 1;
   static constexpr std::uint32_t ID_NONE  = 2;
 
-  static const Block NONE;
-  static const Block STONE;
-  static const Block GRASS;
-
   std::uint32_t id          : 27;
   std::uint32_t sky         : 1;
   std::uint32_t light_level : 4;
 };
-
-inline const Block Block::STONE = Block{ .id = ID_STONE };
-inline const Block Block::GRASS = Block{ .id = ID_GRASS };
-inline const Block Block::NONE  = Block{ .id = ID_NONE };
 
