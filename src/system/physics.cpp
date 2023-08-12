da2fe51f52ed8f2a9dd2ea9fc1763a24bb47a734
KenKwok <kenkinming2002@gmail.com>
2023-08-13 03:50:12 +0800

    Fix Swept AABB

--- src/system/physics.cpp
+++ src/system/physics.cpp
@@ -32,8 +32,11 @@ private:
 
   static std::optional<SweptAABBResult> swept_aabb(AABB box1, AABB box2, glm::vec3 direction)
   {
-    glm::vec3 ds1 = box2.position - (box1.position + box1.dimension); // Distance to enter
-    glm::vec3 ds2 = (box2.position + box2.dimension) - box1.position; // Distance to leave
+    if(direction == glm::vec3(0.0f))
+      return std::nullopt;
+
+    glm::vec3 ds_in  = box2.position - (box1.position + box1.dimension); // Distance to enter
+    glm::vec3 ds_out = (box2.position + box2.dimension) - box1.position; // Distance to leave
 
     SweptAABBResult result = {
       .t_in  = -std::numeric_limits<float>::infinity(),
@@ -41,108 +44,99 @@ private:
     };
     for(int i=0; i<3; ++i)
     {
-      float d1 = ds1[i];
-      float d2 = ds2[i];
+      float d_in  = ds_in[i];
+      float d_out = ds_out[i];
       if(direction[i] != 0.0f)
       {
-        float t1 = d1 / direction[i];
-        float t2 = d2 / direction[i];
-        if(t1 > t2)
-          std::swap(t1, t2);
+        float t1 = d_in  / direction[i];
+        float t2 = d_out / direction[i];
 
-        if(result.t_in < t1)
+        float t_min = std::min(t1, t2);
+        float t_max = std::max(t1, t2);
+
+        if(result.t_in < t_min)
         {
-          result.t_in         = t1;
+          result.t_in         = t_min;
           result.normal_in    = {};
-          result.normal_in[i] = direction[i] > 0.0f ? -1 : 1;
+          result.normal_in[i] = direction[i] > 0.0f ? -1.0f : 1.0f;
         }
 
-        if(result.t_out > t2)
+        if(result.t_out > t_max)
         {
-          result.t_out         = t2;
+          result.t_out         = t_max;
           result.normal_out    = {};
-          result.normal_out[i] = direction[i] > 0.0f ? -1 : 1;
+          result.normal_out[i] = direction[i] > 0.0f ? 1.0f : -1.0f;
         }
       }
-      else
-        if(d1 * d2 > 0.0f)
-          return std::nullopt;
+      else if(d_in * d_out >= 0.0f)
+        return std::nullopt;
     }
     return result;
   }
 
-  static std::optional<SweptAABBResult> swept_aabb_fold(SweptAABBResult lhs, SweptAABBResult rhs)
-  {
-    SweptAABBResult result = {};
-    if(lhs.t_in  < rhs.t_in)  { result.t_in  = lhs.t_in;  result.normal_in  = lhs.normal_in;  } else { result.t_in  = rhs.t_in;  result.normal_in  = rhs.normal_in; }
-    if(lhs.t_out < rhs.t_out) { result.t_out = lhs.t_out; result.normal_out = lhs.normal_out; } else { result.t_out = rhs.t_out; result.normal_out = rhs.normal_out; }
-    return result;
-  }
-
-  static std::optional<SweptAABBResult> swept_aabb_fold(std::optional<SweptAABBResult> lhs, std::optional<SweptAABBResult> rhs)
-  {
-    if(lhs && rhs) return swept_aabb_fold(*lhs, *rhs);
-    if(lhs && !rhs) return lhs;
-    if(rhs && !lhs) return rhs;
-    return std::nullopt;
-  }
-
 private:
-  static void entity_update_physics(Entity& entity, float dt)
+  static void entity_update_physics(const Dimension& dimension, Entity& entity, float dt)
   {
     float friction = entity.grounded ? FRICTION_GROUNDED : FRICTION_AIR;
     entity.apply_force(-friction * entity.velocity,             dt);
     entity.apply_force(-GRAVITY  * glm::vec3(0.0f, 0.0f, 1.0f), dt);
-    entity.transform.position += dt * entity.velocity;
-  }
 
-  static void entity_resolve_collisions(Entity& entity, const Dimension& dimension)
-  {
-    entity.collided = false;
+    glm::vec3 direction = dt * entity.velocity;
 
-    Entity original_entity = entity;
+    glm::vec3 corner1 = entity.transform.position                                  ;
+    glm::vec3 corner2 = entity.transform.position                       + direction;
+    glm::vec3 corner3 = entity.transform.position + entity.bounding_box            ;
+    glm::vec3 corner4 = entity.transform.position + entity.bounding_box + direction;
 
-    for(int i=0; i<MAX_COLLISION_ITERATION; ++i)
+    glm::ivec3 corner_min = glm::floor(glm::min(glm::min(glm::min(corner1, corner2), corner3), corner4));
+    glm::ivec3 corner_max = glm::ceil (glm::max(glm::max(glm::max(corner1, corner2), corner3), corner4));
+
+    struct Item
     {
-      std::optional<SweptAABBResult> result;
+      glm::ivec3 position;
+      float      distance;
+    };
+    std::vector<Item> items;
 
-      glm::ivec3 corner1 = glm::floor(entity.transform.position);
-      glm::ivec3 corner2 = -glm::floor(-(entity.transform.position + entity.bounding_box))-1.0f;
-      for(int z = corner1.z; z<=corner2.z; ++z)
-        for(int y = corner1.y; y<=corner2.y; ++y)
-          for(int x = corner1.x; x<=corner2.x; ++x)
-          {
-            glm::ivec3   position = glm::ivec3(x, y, z);
-            const Block* block    = dimension.get_block(position);
-            if(block && block->id != Block::ID_NONE)
-            {
-              AABB entity_aabb = { .position = entity.transform.position, .dimension = entity.bounding_box, };
-              AABB block_aabb  = { .position = position,                  .dimension = glm::vec3(1.0f),     };
-              glm::vec3 direction = entity.velocity;
-              result = swept_aabb_fold(result, swept_aabb(entity_aabb, block_aabb, direction));
-            }
-          }
+    // We store all voxel that may be in our path and sort them by distance.
+    // The idea comes from the video - Arbitrary Rectangle Collision Detection &
+    // Resolution - Complete! by javidx9 at
+    // https://www.youtube.com/watch?v=8JJ-4JgR7Dg.
+    for(int z = corner_min.z; z<=corner_max.z; ++z)
+      for(int y = corner_min.y; y<=corner_max.y; ++y)
+        for(int x = corner_min.x; x<=corner_max.x; ++x)
+          items.push_back(Item{
+            .position = glm::ivec3(x, y, z),
+            .distance = glm::length(glm::vec3(x, y, z) - entity.transform.position),
+          });
 
-      if(result && result->t_in < result->t_out && result->t_in * result->t_out < 0.0f)
+    std::sort(items.begin(), items.end(), [](const Item& lhs, const Item& rhs) { return lhs.distance < rhs.distance; });
+
+    for(const Item& item : items)
+    {
+      if(const Block* block = dimension.get_block(item.position); block && block->id != Block::ID_NONE)
       {
-        entity.transform.position += glm::dot(entity.velocity, result->normal_in) * result->normal_in * result->t_in; // Backtrack in normal direction
-        entity.velocity           -= glm::dot(entity.velocity, result->normal_in) * result->normal_in;                // Cancel velocity in normal direction
-        entity.collided = true;
-        if(result->normal_in.z > 0.0f)
-          entity.grounded = true;
+        AABB entity_aabb = { .position = entity.transform.position, .dimension = entity.bounding_box, };
+        AABB block_aabb  = { .position = item.position,             .dimension = glm::vec3(1.0f),     };
+        if(std::optional<SweptAABBResult> result = swept_aabb(entity_aabb, block_aabb, direction))
+          if(0.0f <= result->t_in && result->t_in <= 1.0f)
+          {
+            direction       -= glm::dot(direction,       result->normal_in) * result->normal_in * (1.0f - result->t_in);
+            entity.velocity -= glm::dot(entity.velocity, result->normal_in) * result->normal_in;
+
+            entity.collided = true;
+            if(result->normal_in.z > 0.0f)
+              entity.grounded = true;
+          }
       }
-      else
-        return;
     }
 
-    spdlog::warn("Collision resolution failed");
-    entity = original_entity;
+    entity.transform.position += direction;
   }
 
   void on_update(World& world, float dt) override
   {
-    entity_update_physics(world.player, dt);
-    entity_resolve_collisions(world.player, world.dimension);
+    entity_update_physics(world.dimension, world.player, dt);
   }
 };
 
