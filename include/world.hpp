924acc5b001f753bb77ee334a794f66ece65c719
KenKwok <kenkinming2002@gmail.com>
2023-08-16 01:24:57 +0800

    Add world_config.hpp

--- include/world.hpp
+++ include/world.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <camera.hpp>
-#include <entity.hpp>
 #include <dimension.hpp>
+#include <entity.hpp>
+#include <world_config.hpp>
 
 #include <graphics/texture_array.hpp>
 #include <graphics/mesh.hpp>
@@ -13,64 +14,6 @@
 #include <optional>
 
 #include <cstddef>
-
-struct TerrainLayerGenerationConfig
-{
-  uint32_t block_id;
-
-  float    base;
-  float    frequency;
-  float    amplitude;
-  float    lacunarity;
-  float    persistence;
-  unsigned octaves;
-};
-
-struct TerrainGenerationConfig
-{
-  std::vector<TerrainLayerGenerationConfig> layers;
-};
-
-struct CavesGenerationConfig
-{
-  unsigned max_per_chunk;
-  unsigned max_segment;
-  float    step;
-
-  float min_height;
-  float max_height;
-
-  float dig_frequency;
-  float dig_amplitude;
-  float dig_lacunarity;
-  float dig_persistence;
-  float dig_octaves;
-
-  float radius;
-  float radius_frequency;
-  float radius_amplitude;
-  float radius_lacunarity;
-  float radius_persistence;
-  float radius_octaves;
-};
-
-struct WorldGenerationConfig
-{
-  std::size_t             seed;
-  TerrainGenerationConfig terrain;
-  CavesGenerationConfig   caves;
-};
-
-struct BlockConfig
-{
-  std::array<std::string, 6> textures;
-};
-
-struct WorldConfig
-{
-  WorldGenerationConfig    generation;
-  std::vector<BlockConfig> blocks;
-};
 
 struct WorldData
 {
