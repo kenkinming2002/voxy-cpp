fbb4af9db57084882d8562394087dca5c4c1295c
KenKwok <kenkinming2002@gmail.com>
2023-08-23 20:43:59 +0800

    Cleanup

--- src/graphics/wireframe_renderer.cpp
+++ src/graphics/wireframe_renderer.cpp
@@ -46,21 +46,22 @@ namespace graphics
         graphics::as_bytes(vertices)
         );
   }
+
+  void WireframeRenderer::render_cube(const Camera& camera, glm::vec3 position, glm::vec3 dimension, float thickness)
+  {
+    glm::mat4 view       = camera.view();
+    glm::mat4 projection = camera.projection();
+
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, position);
+    model = glm::scale    (model, dimension);
+
+    glm::mat4 MVP = projection * view * model;
+
+    glUseProgram(m_shader_program->id());
+    glUniformMatrix4fv(glGetUniformLocation(m_shader_program->id(), "MVP"), 1, GL_FALSE, glm::value_ptr(MVP));
+    glLineWidth(thickness);
+    m_cube_mesh->draw_lines();
+  }
 }
 
-void graphics::WireframeRenderer::render_cube(const Camera& camera, glm::vec3 position, glm::vec3 dimension, float thickness)
-{
-  glm::mat4 view       = camera.view();
-  glm::mat4 projection = camera.projection();
-
-  glm::mat4 model = glm::mat4(1.0f);
-  model = glm::translate(model, position);
-  model = glm::scale    (model, dimension);
-
-  glm::mat4 MVP = projection * view * model;
-
-  glUseProgram(m_shader_program->id());
-  glUniformMatrix4fv(glGetUniformLocation(m_shader_program->id(), "MVP"), 1, GL_FALSE, glm::value_ptr(MVP));
-  glLineWidth(thickness);
-  m_cube_mesh->draw_lines();
-}
